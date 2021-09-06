d76671ea93c2ba5a77141697e2e618aefbdf1e7d
Ebrima Sise <siseebrima@gmail.com>
2021-09-06 16:35:39 +0000

    s is the end of part2

--- part2/part2-tut/src/App.jsx
+++ part2/part2-tut/src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from "react";
 import Note from "./components/Note";
+import Notification from "./components/Notification";
 // import axios from "axios";
 import noteService from "./services/notes";
 
@@ -8,6 +9,7 @@ const App = (props) => {
   const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("");
   const [showAll, setShowAll] = useState(true);
+  const [errorMessage, setErrorMessage] = useState("some error happened");
 
   useEffect(() => {
     noteService.getAll().then((response) => {
@@ -56,8 +58,12 @@ const App = (props) => {
         setNotes(notes.map((note) => (note.id !== id ? note : response)));
       })
       .catch((error) => {
-        alert(`the note '${note.content}' was already deleted from the server`);
-        console.log(error.message);
+        setErrorMessage(
+          `Note '${note.content}' was already removed from the server`
+        );
+        setTimeout(() => {
+          setErrorMessage(null);
+        }, 5000);
         setNotes(notes.filter((n) => n.id !== id));
       });
 
@@ -70,6 +76,7 @@ const App = (props) => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={handleToggle}>
           show {showAll ? "important" : "all"}
