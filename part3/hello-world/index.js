7121f206c868ac96668608e6b64864ac5f592940
Ebrima Sise <siseebrima@gmail.com>
2021-09-09 14:37:25 +0000

    first part of node and express

---
+++ part3/hello-world/index.js
@@ -0,0 +1,85 @@
+import express from "express";
+// import http from "http";
+
+const app = express();
+app.use(express.json());
+
+let notes = [
+  {
+    id: 1,
+    content: "HTML is easy",
+    date: "2019-05-30T17:30:31.098Z",
+    important: true,
+  },
+  {
+    id: 2,
+    content: "Browser can execute only Javascript",
+    date: "2019-05-30T18:39:34.091Z",
+    important: false,
+  },
+  {
+    id: 3,
+    content: "GET and POST are the most important methods of HTTP protocol",
+    date: "2019-05-30T19:20:14.298Z",
+    important: true,
+  },
+];
+
+app.get("/", (req, res) => {
+  res.send("<h1>Hello Ebrima</h1>");
+});
+
+app.get("/api/notes", (req, res) => {
+  res.json(notes);
+});
+
+app.get("/api/notes/:id", (request, response) => {
+  const id = Number(request.params.id);
+  const note = notes.find((note) => note.id === id);
+
+  //   if (note) {
+  //     response.json(note);
+  //   } else {
+  //     response.status(404).end();
+  //   }
+  note
+    ? response.json(note)
+    : response
+        .status(404)
+        .send("opps! The resource you are looking for does not exist");
+});
+
+app.delete("/api/notes/:id", (req, res) => {
+  const id = Number(req.params.id);
+  notes = notes.filter((n) => n.id !== id);
+  res.status(204).end();
+});
+
+const generateId = () => {
+  const maxId = notes.length > 0 ? Math.max(...notes.map((n) => n.id)) : 0;
+  return maxId + 1;
+};
+
+app.post("/api/notes", (request, response) => {
+  const body = request.body;
+
+  if (!body.content) {
+    return response.status(400).json({
+      error: "content missing",
+    });
+  }
+
+  const note = {
+    content: body.content,
+    important: body.important || false,
+    date: new Date(),
+    id: generateId(),
+  };
+  notes = notes.concat(note);
+  response.json(note);
+});
+
+const port = 3001;
+app.listen(port, () => {
+  console.log(`Ebrima, the server is listening on port ${port}`);
+});
